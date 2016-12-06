317fd1d3db1d19b06f6fc4cde8980cefd28fd66e
Hiroyuki Sato <sato@cloudpack.jp>
2016-12-06 16:56:07 +0900

    variables

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "cloudfront_oai_id" {
-    value = "aws_cloudfront_origin_access_identity.asset_origin_access_identity.id"
+    value = "${aws_cloudfront_origin_access_identity.asset_origin_access_identity.id}"
 }
 
 output "cloudfront_oai_iam_arn" {
-    value = "aws_cloudfront_origin_access_identity.asset_origin_access_identity.iam_arn"
+    value = "${aws_cloudfront_origin_access_identity.asset_origin_access_identity.iam_arn}"
 }
 
 output "cloudfront_distribution_id" {
-    value = "aws_cloudfront_distribution.asset_cloudfront_distribution.id"
+    value = "${aws_cloudfront_distribution.asset_cloudfront_distribution.id}"
 }
 
 output "cloudfront_domain_name" {
-    value = "aws_cloudfront_distribution.asset_cloudfront_distribution.domain_name"
+    value = "${aws_cloudfront_distribution.asset_cloudfront_distribution.domain_name}"
 }
 
 output "cloudfront_hosted_zone_id" {
-    value = "aws_cloudfront_distribution.asset_cloudfront_distribution.hosted_zone_id"
+    value = "${aws_cloudfront_distribution.asset_cloudfront_distribution.hosted_zone_id}"
 }
